3cc26bf405a5494de5679f48d317f6509ba855b8
sanudas2018 <sanu.das08103@gmail.com>
2024-09-23 00:50:46 +0600

    Install (Laravel Jetstream) and migrate User/Customer table, and CSRF OFF

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -12,13 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            // Recommend Part
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('role')->default('customer');
+            $table->string('address');
             $table->string('password');
+
+            // External Part
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
-            $table->timestamps();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
+
+            // Default Timestamp Field Crate
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
